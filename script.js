7d6e97bddc70343f35afeea641ae07e429c3b5ac
mardiyahpn <150130367+mardiyahpn@users.noreply.github.com>
2023-11-07 18:59:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function Login()
+    var done = 0 ;
+    var usename = document.login.username.value;
+    var password = document.login.password.value;
+
+    if (username == "himasi" && password == "himasi"){
+        if(confirm("Apakah anda ingin login?")){
+            window.location.href = "https://github.com/";
+        } else {
+            alert("Membatalkan login !");
+        } else {
+            if(done==0){   
+            }
+        }   alert("Username/password Salah!");
+    } 
